19575af6c998a0f3ac31a89ce8dbbe1faed3d402
gtkatakura <gt.katakura@gmail.com>
2017-07-20 21:24:52 -0300

    (new doc) add esdoc for the first method

--- src/array/flatten.js
+++ src/array/flatten.js
@@ -1,3 +1,13 @@
+/**
+ * Flattens `array` a single level deep.
+ *
+ * @param {Array} array The array to flatten.
+ * @returns {Array} Returns the new flattened array.
+ * @example
+ *
+ * flatten([1, [2, [3]])
+ * // => [1, 2, [3]]
+ */
 const flatten = array => [].concat(...array);
 
 export default flatten;
